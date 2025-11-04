af8cc54002455022c27e97b3287ff1bb947e877d
Mudrov Maksim <mudrov010203@gmail.com>
2025-11-04 20:52:32 +0300

    Добавил ФИО и группу в Program

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine($"Текущая дата: {DateTime.Now:dd,MM.yyyy}");
+Console.WriteLine("Группа: ИС-22А");
+Console.WriteLine("ФИО: Мудров Максим Андреевич");
